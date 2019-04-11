04162ab137a79dec930fdf7e9f969e1f4452507d
Alice Hui <alice.hui@digitcube.com>
2019-04-11 11:07:22 +0800

    feat: change to use HashRouter for deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { HashRouter as Router, Switch } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
